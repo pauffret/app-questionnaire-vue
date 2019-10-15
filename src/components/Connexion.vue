2a48262119db8e6afccdd9dbfb521a3cbc278f3c
PIERRE <pierreauffret@free.fr>
2019-10-15 18:07:42 +0200

    connexion

--- src/components/Connexion.vue
+++ src/components/Connexion.vue
@@ -1,13 +1,63 @@
 <template>
-    $END$
+    <div class="formulaireConnexion">
+        <h1>EEV SAS</h1><br>
+        <h2>Formulaire de connexion</h2><br>
+        <span v-if="showError" class="errorMessage">Les champs doivent être tous remplis !</span>
+        <b-input-group prepend="Nom" class="mt-3">
+            <b-form-input v-model="nom" placeholder="Nom"></b-form-input>
+        </b-input-group>
+        <b-input-group prepend="Prénom" class="mt-3">
+            <b-form-input v-model="prenom" placeholder="Prénom"></b-form-input>
+        </b-input-group>
+        <b-input-group prepend="Société" class="mt-3">
+            <b-form-input v-model="societe" placeholder="Société"></b-form-input>
+        </b-input-group>
+
+        <b-button block variant="primary" class="connectButton" @click="connected">Commencer le test</b-button>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Connexion"
+        name: "Connexion",
+        data() {
+            return {
+                nom: "",
+                prenom: "",
+                societe: "",
+                showError: false
+            }
+        },
+        methods: {
+            connected() {
+                if (this.nom.trim() != "" && this.prenom.trim() != "" && this.societe.trim() != "") {
+                    this.$router.push({
+                        path: 'questionnaire',
+                        query: {
+                            nom: this.nom,
+                            prenom: this.prenom,
+                            societe: this.societe
+                        }
+                    })
+                } else {
+                    this.showError = true
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .connectButton {
+        margin-top: 4%;
+    }
+    .errorMessage {
+        font-weight: bold;
+        color: red;
+        font-size: 18px;
+    }
+    .formulaireConnexion{
+        margin-left: 30%;
+        margin-right: 30%;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VuePouchDB from 'vue-pouch-db';
+// import './custom.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
+Vue.use(VuePouchDB);
 
 new Vue({
   router,
